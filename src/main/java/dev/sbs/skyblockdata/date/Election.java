2003ba857e568806f6d51e497dbbb87b85c18404
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-04-27 05:26:49 -0400

    Add Election and SpecialElection date types
    Election models a regular SkyBlock mayoral cycle (voting + term windows
    keyed off LATE_SUMMER 27 / LATE_SPRING 27), and SpecialElection covers
    out-of-band cycles. Both implement PostInit so deserialized instances
    re-derive their SkyBlockDate windows from the year alone.

---
+++ src/main/java/dev/sbs/skyblockdata/date/Election.java
@@ -0,0 +1,72 @@
+package dev.sbs.skyblockdata.date;
+
+import dev.simplified.gson.PostInit;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+public class Election implements PostInit {
+
+    private int year;
+    private transient Cycle voting;
+    private transient Cycle term;
+
+    public Election(int year) {
+        this.year = year;
+        this.postInit();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Election election = (Election) o;
+
+        return this.getYear() == election.getYear()
+            && Objects.equals(this.getVoting(), election.getVoting())
+            && Objects.equals(this.getTerm(), election.getTerm());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getYear(), this.getVoting(), this.getTerm());
+    }
+
+    @Override
+    public void postInit() {
+        this.voting = new Cycle(
+            new SkyBlockDate(this.getYear(), Season.LATE_SUMMER, 27, 0),
+            new SkyBlockDate(this.getYear() + 1, Season.LATE_SPRING, 27, 0)
+        );
+        this.term = new Cycle(
+            new SkyBlockDate(this.getYear() + 1, Season.LATE_SPRING, 27, 0),
+            new SkyBlockDate(this.getYear() + 2, Season.LATE_SPRING, 27, 0)
+        );
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Election{year=%d, voting=%s, term=%s}", this.getYear(), this.getVoting(), this.getTerm());
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Cycle {
+
+        private final @NotNull SkyBlockDate start;
+        private final @NotNull SkyBlockDate end;
+
+        @Override
+        public String toString() {
+            return String.format("Cycle{start=%s, end=%s}", this.getStart(), this.getEnd());
+        }
+
+    }
+
+}
---
+++ src/main/java/dev/sbs/skyblockdata/date/SpecialElection.java
@@ -0,0 +1,44 @@
+package dev.sbs.skyblockdata.date;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@Getter
+public class SpecialElection extends Election {
+
+    private final @NotNull String specialMayor;
+
+    public SpecialElection(int year, @NotNull String specialMayor) {
+        super(year);
+        this.specialMayor = specialMayor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        SpecialElection that = (SpecialElection) o;
+
+        return Objects.equals(this.getSpecialMayor(), that.getSpecialMayor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), this.getSpecialMayor());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "SpecialElection{specialMayor='%s', year=%d, voting=%s, term=%s}",
+            this.getSpecialMayor(),
+            this.getYear(),
+            this.getVoting(),
+            this.getTerm()
+        );
+    }
+
+}
